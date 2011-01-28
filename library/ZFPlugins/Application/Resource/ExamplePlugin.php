2f6f37e72ca39636c79cc64d5b5af730c2aa5799
Rudger <rudgergravestein@gmail.com>
2011-01-28 04:20:19 +0100

    added ZFPlugins_Application_Resource_ExamplePlugin

---
+++ library/ZFPlugins/Application/Resource/ExamplePlugin.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * 
+ * @example
+ * add to the /application/config/application.ini:
+ * [APPLICATION_ENV]
+ * pluginPaths.My_Resource = APPLICATION_PATH "/resources"
+ * resources.foo[] = "value"
+ * ; or
+ * resources.foo.key = "value"
+ * 
+ * Access the ExamplePlugin from
+ *  - ActionController 
+ *  - $bootstrap = $this->getInvokeArg('bootstrap');
+ *  - $examplePlugin = $bootstrap->getResource('ExamplePlugin');
+ * 
+ * @author [rg] r.gravestein <rudgergravestein@gmail.com>, 28 jan. 2011
+ *
+ */
+class ZFPlugins_Application_Resource_ExamplePlugin extends Zend_Application_Resource_ResourceAbstract
+{
+    /**
+     * (non-PHPdoc)
+     * @see Zend_Application_Resource_Resource::init()
+     */
+    public function init ()
+    {
+        $bootstrap = $this->getBootstrap();
+        $layout = $bootstrap->getResource('layout');
+        $view = $layout->getView();
+        
+        $config = Zend_Registry::get('config');
+        $view->config = $config;
+        
+        $foo = new self();
+        return $foo;
+        
+    }
+}
